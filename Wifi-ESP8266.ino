0374e6c5668eb2436d076fb74026efa9687937f3
Stiven Rodriguez <merq.rodriguez@udla.edu.co>
2019-04-13 23:19:10 -0500

    updated

--- Wifi-ESP8266.ino
+++ Wifi-ESP8266.ino
@@ -1,24 +1,55 @@
+/*
+      
+      WiFi.begin(ssid, password = null, channel = 0, bssid = null, connect = true)
+
+      ssid:       nombre de la red WiFi a la que nos queremos conectar (max 32 char)
+      password:   opcional, contraseña (mínimo 8 char y máximo 64)
+      channel:    opcional, el canal de WiFi a emplear
+      bssid:      opcional, dirección MAC del punto de acceso
+      connect:    indica si queremos conectar inmediatamente (si es false, sólo guarda los parámetros)
+*/
+
+/*
+      WiFi.status(). Esta variable de estado del ESP8266 puede ser:
+
+      WL_CONNECTED:	      Conexión establecida
+      WL_NO_SSID_AVAIL:	  El SSID no se encuentra
+      WL_CONNECT_FAILED:	Contraseña incorrecta
+      WL_IDLE_STATUS:	    Wi-Fi está cambiando entre estados
+      WL_DISCONNECTED:	  El ESP8266 no está configurado en modo STA
+*/
+
+/*
+      WIFI_STA:	Estacion
+      WIFI_AP:	Access point
+      WIFI_AP_STA:	Station+Acces Point
+*/
+
 #include <ESP8266WiFi.h>
  
 // Sustituir con datos de la red a la que se va a conectar
 const char* ssid     = "La nueva Fraternidad";
 const char* password = "estanoesunaclave";
  
-void setup()
-{
+void setup(){
   Serial.begin(115200);
   delay(10);
   
+  //La función WiFi.mode(WIFI_STA) es opcional en este ejemplo. 
+  //Sirve para dejar únicamente el modo STA y desactivar el AP
   WiFi.mode(WIFI_STA);
+
+  //  Guarda los credenciales indicados en la memoria flash no volátil
+  //  (que se mantiene incluso aunque carguemos otro programa). Ante un fallo de conexión,
+  //  el ESP8266 se conectará automáticamente al último punto de acceso cuando esté disponible.
   WiFi.begin(ssid, password);
   Serial.print("Conectando a:\t");
   Serial.println(ssid); 
  
   // Esperar a que nos conectemos
-  while (WiFi.status() != WL_CONNECTED) 
-  {
-    delay(200);
-   Serial.print('.');
+  while (WiFi.status() != WL_CONNECTED) {
+     delay(200);
+     Serial.print('.');
   }
  
   // Mostrar mensaje de exito y dirección IP asignada
@@ -29,6 +60,4 @@ void setup()
   Serial.println(WiFi.localIP());
 }
  
-void loop() 
-{
-}
+void loop() {}
